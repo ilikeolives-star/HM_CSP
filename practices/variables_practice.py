9b43b767a0ed920e93b4152b33d46c7fc1aed78f
github.ilikeolives-star <hailey.munian@ucas-edu.net>
2026-01-22 14:32:16 -0700

    finished notes

---
+++ practices/variables_practice.py
@@ -0,0 +1,25 @@
+# HM Variables Practice
+
+name = "Hailey" 
+
+number = 4
+
+big_number = 999
+
+favorite_color = "purple"
+
+breafast = "biscuits"
+
+age = 14
+
+eye_color = "brown"
+
+year = 2026
+
+school = "Ucas"
+
+subject = "math"
+
+print(name, number, big_number)
+
+
