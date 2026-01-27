8d96c9c53e0f6d1234bb8b7112ece698fd532532
ilikeolives-star <hailey.munian@ucas-edu.net>
2026-01-27 04:04:08 +0000

    half way

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -1,5 +1,6 @@
 # HM financial calculator
 monthly_income = input("What is your monthly income? $")
-
-
+monthly_rent = input("How much is the rent monthly: %")/100
+monthly_utilities = input("How much is the utilities monthly: %")/100
+month_groceries = input("How much is the groceries monthly: %")/100
 
