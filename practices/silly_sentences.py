1f88354f52139f16002653930297d02b100f3be1
github.ilikeolives-star <hailey.munian@ucas-edu.net>
2026-01-28 14:42:24 -0700

    silly sentences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,5 @@
+# HM P7 Silly sentences
+noun1 = input("What is a randon noun: ").strip().lower()
+adjective = input("What is a random adjective: ").strip().lower()
+noun2 = input("What is another random noun: ").strip().lower()
+print("After hiding the painting in his " + noun1 + " for two years, he grew " + adjective + " and tried to sell it to a/an " + noun2 + " in Florence, but was caught.")
