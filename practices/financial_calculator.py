216ab7e1a7663fd8d7c9e366a3631e8ad2408073
ilikeolives-star <hailey.munian@ucas-edu.net>
2026-01-28 00:55:41 +0000

    done

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -3,4 +3,4 @@ monthly_income = input("What is your monthly income? $")
 monthly_rent = input("How much is the rent monthly: %")/100
 monthly_utilities = input("How much is the utilities monthly: %")/100
 month_groceries = input("How much is the groceries monthly: %")/100
-print(f"This is how much of your income your groceries take in percent. month_groceries / monthly_income")
+groceries =  print(f"This is how much of your income your groceries take in percent. month_groceries / monthly_income")
