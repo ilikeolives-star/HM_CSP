a8c1048dfcff3a6a1590e761f4a12f5314a38b4c
github.ilikeolives-star <hailey.munian@ucas-edu.net>
2026-02-05 14:42:31 -0700

    e

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -1,6 +1,13 @@
 # HM financial calculator
 monthly_income = input("What is your monthly income? $")
-monthly_rent = input("How much is the rent monthly: %")/100
-monthly_utilities = input("How much is the utilities monthly: %")/100
-month_groceries = input("How much is the groceries monthly: %")/100
-groceries =  print(f"This is how much of your income your groceries take in percent. month_groceries / monthly_income")
+monthly_rent = input("How much is the rent monthly: $")
+monthly_utilities = input("How much is the utilities monthly: $")
+month_groceries = input("How much is the groceries monthly: $")
+monthly_transportation = input("How much is monthly transportation: $")
+
+savings = monthly_income * 0.1
+money = monthly_income - monthly_rent - monthly_utilities - month_groceries - monthly_transportation - savings
+
+print(f"Your rent is $ {monthly_rent} and that is 40 % of your income")
+print(f"Your utilities is $ {monthly_utilities} and that is 7 % of your income")
+print(f"Your grocieries is $ {month_groceries} and that is 7 % of your income")
