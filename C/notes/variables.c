334d21ee391cb74180cebacc190e5d6b88e2b688
ilikeolives-star <hailey.munian@ucas-edu.net>
2026-02-18 14:42:23 -0700

    eee

---
+++ C/notes/variables.c
@@ -0,0 +1,16 @@
+// This is a comment in C!
+#include <stdio.h> // Lets us use inputs and outputs
+
+int main(){
+    //variables examples
+    int age = 9999;
+    float gpa = 3.98;
+    char grade = 'A'; // single letter uses single quotes
+    char name[] = "Eric";
+
+    printf("It worked!");
+    printf("%d\n", age);
+    printf("Your gpa is: %f\n", gpa);
+    printf("You have a %c in computer science\n", grade);
+    return 0; // required last line
+}
