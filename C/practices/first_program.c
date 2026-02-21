ec0c71e940eac626f86b1988a490eed45b8c172b
ilikeolives-star <hailey.munian@ucas-edu.net>
2026-02-21 02:48:13 +0000

    first attempt?

---
+++ C/practices/first_program.c
@@ -0,0 +1,17 @@
+// HM P7 first program!
+#include <stdio.h>
+int main(){
+  
+char Name[30];
+
+
+printf("whats your name: \n");
+
+
+scanf("%s", Name);
+
+
+printf("Hello %s", Name);
+     return 0;
+
+}
