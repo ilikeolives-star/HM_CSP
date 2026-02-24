699f48f1082d772dfd561b1264aecba6d6422ea3
ilikeolives-star <hailey.munian@ucas-edu.net>
2026-02-24 14:43:43 -0700

    gfa

--- C/practices/financial_calculator.c
+++ C/practices/financial_calculator.c
@@ -7,23 +7,23 @@ int main(){
     int groceries;
     int transportation; 
 
-    printf("Your monthly income will be: %d\n");
+    printf("Your monthly income will be: \n");
     scanf("%d", &income);
-    printf("Rent is: %d\n");
+    printf("Rent is: \n");
     scanf("%d", &rent);
    
-    printf("Rent is %d of your income\n", (int) rent/income * 100);
-    printf("utilities is: %d\n");
+    printf("Rent is %d of your income \n", (int) rent/income * 100);
+    printf("utilities is: \n");
     scanf("%d, &utilities");
 
     printf("Utilities is %d of your income\n", (int) utilities/income * 100);
  
-    printf("Groceries is: %d\n");
+    printf("Groceries is: \n");
     scanf("%d", &groceries);
 
     printf("Groceries is %d of your income\n", (int) groceries/income * 100);
 
-    printf("Transportation is: %d\n");
+    printf("Transportation is: \n");
     scanf("%d", &transportation);
     printf("Transportation is %d of your income\n", (int) transportation/income * 100);
     
