546c5cb54420220c3020496e417906f2398d2af7
ilikeolives-star <hailey.munian@ucas-edu.net>
2026-03-05 03:45:52 +0000

    Yayyyyy

--- C/practices/fizzbuzz.c
+++ C/practices/fizzbuzz.c
@@ -1,2 +1,17 @@
 // HM p7 fizzbuzz
 #include <stdio.h>
+int main(void) {
+    for (int i = 1; i <= 50; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz\n");
+        } else if (i % 3 == 0) {
+            printf("Fizz\n");
+        } else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
+
